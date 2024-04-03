17b2b60549c0d87240e95436d26e75e68f99d80a
Daniel IRYIVUZE <139581457+Daniel-IRYIVUZE@users.noreply.github.com>
2024-04-03 13:33:06 +0200

    Made by Daniel

--- src/Todo/Addtodo.jsx
+++ src/Todo/Addtodo.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import {useState} from 'react'
+import React, { useState } from 'react';
 
-export const AddTodo = ({todos, setTodos}) => {
-  const [todoname, setTodoName] = useState("")
-  const addtodos = ()=>{
-    if (todoname!==""){
-      setTodos([...todos,{id:todos.length+1,name:todoname,status:false}])
-      setTodoName("")
+export const AddTodo = ({ todos, setTodos }) => {
+  const [todoname, setTodoName] = useState('');
+
+  const addTodos = () => {
+    if (todoname !== '') {
+      setTodos([...todos, { id: todos.length + 1, name: todoname, status: false }]);
+      setTodoName('');
+    } else {
+      alert('Kindly add an activity to do!!!');
     }
-    else{
-      alert("Kindly add an activity to do !!!")
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      addTodos();
     }
-  }
+  };
+
   return (
     <>
-    <div className='bg-white p-2 w-full mt-29 rounded-md w-30'>
-      <h2 className='text-2xl text-green-500 font-bold rounded'> Add to Do </h2>
-    <input onChange={(events)=>setTodoName(events.target.value)} value={todoname} type='text' className='p-2 bg-slate-200 w-full mt-2 outline-none rounded-md' placeholder='Enter Activity'></input>
-    <button onClick={()=>addtodos()} className='p-2 bg-green-400 text-grey-400 w-full mt-3 font-bold rounded-md'> Add Todo </button>
-    </div>
+      <div className='bg-white p-2 w-full mt-29 rounded-md w-30'>
+        <h2 className='text-2xl text-slate-600 font-bold rounded'> Add Task to Do </h2>
+        <input
+          onChange={(event) => setTodoName(event.target.value)}
+          onKeyPress={handleKeyPress}
+          value={todoname}
+          type='text'
+          className='p-2 bg-slate-300 w-full mt-2 outline-none rounded-md'
+          placeholder='Enter Activity'
+        />
+        <button
+          onClick={addTodos}
+          className='p-2 bg-slate-600 text-white w-full mt-3 font-bold rounded-md'
+        >
+          Add now
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
